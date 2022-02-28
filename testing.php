537b6475cda186b6197743a83774f722733aa325
ARJUN REDDY <52547403+ankur619158@users.noreply.github.com>
2022-03-01 00:16:01 +0530

    Add files via upload

---
+++ testing.php
@@ -0,0 +1,9 @@
+<?php
+include('connection.php');
+$obj = new operation();
+$condition = array('id'=>'5','name'=>'vishnu_parsad');
+/*$result = $obj->insertData($condition);*/
+/*$result = $obj->getData($condition);*/
+/*$result = $obj->delteData($condition);*/
+/*$result = $obj->updateData($condition , 'id','5'); */
+?>
